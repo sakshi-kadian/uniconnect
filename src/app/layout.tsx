ba02f4e5ab6dc79892bf4a9827b622e8a8c18201
Sakshi <kadiansakshi.sk@gmail.com>
2025-09-02 23:15:37 +0530

    initial commit

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,7 +5,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
     <html lang="en">
       <head>
         <link rel="icon" href="/favicon.ico" />
-        <title>UniConnect</title>
+        <title>UniVerse</title>
       </head>
       <body className="bg-gray-50">
         {children}
